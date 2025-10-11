987980645d772cb64da3fa83434ad2cd6b679f22
Dalia Marzouk <69721475+daliaMarzouk90@users.noreply.github.com>
2025-10-11 08:47:48 +0300

    Add initial implementation of main.py with dotenv integration

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
+def main():
+    print("Hello from lctutorial!")
+
+
+if __name__ == "__main__":
+    main()
